f449d1b67010ebc17e7b4a0487a84591874fa291
unknown <hmsamarasinghe2003@gmail.com>
2024-06-11 09:13:00 +0530

    first commit

---
+++ math.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class math{
+    public static void main(String[] args){
+        Scanner x=new Scanner(System.in);
+
+        double age=x.nextDouble();
+        System.out .println(age);
+        
+
+
+
+
+    }
+}
